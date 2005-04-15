ab2c262ca56d8495884169762223528a1692913d
Petr Baudis <pasky@ucw.cz>
2005-04-15 21:02:14 +0200

    Introduced git init, which will initialize a fresh repository properly.

---
+++ gitinit.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# Initialize a GIT repository.
+# Copyright (c) Petr Baudis, 2005
+
+
+die () {
+	echo gitinit.sh: $@ >&2
+	exit 1
+}
+
+[ -e .git ] && die ".git already exists"
+
+init-db
+mkdir .git/heads .git/tags
