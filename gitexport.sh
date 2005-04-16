ca5385acec502278943ace3b19632f96091ebd53
Petr Baudis <pasky@ucw.cz>
2005-04-16 04:32:40 +0200

    Massive cleanup of git export. It now also makes use of git lntree and avoids messing with our precious current working tree index.

--- gitexport.sh
+++ gitexport.sh
@@ -12,24 +12,14 @@ die () {
 }
 
 destdir=$1
+id=$(gitXnormid.sh $2)
 
-[ "$destdir" == "" ] && die "usage: gitexport.sh <DESTDIR> [<ID>]"
+([ "$destdir" ] && [ "$id" ]) || die "usage: git export DESTDIR [TREE_ID]"
 
-[ -e "$destdir" ] && die "$1 already exists."
+[ -e "$destdir" ] && die "$destdir already exists."
 
-id=$(gitXnormid.sh $2) || exit 1
-
+git lntree $destdir || exit 1
+cd $destdir
 read-tree $id
-
-oldpwd="$(pwd)"
-dircache="$oldpwd"/.git
-
-mkdir "$destdir" || die "Could not create $destdir"
-
-cd "$destdir" || die "Huh? Could not change directory"
-
-ln -s "$dircache" . || die "Could not link .git"
-
-checkout-cache -a && rm .git
-
-cd "$oldpwd" && read-tree $(tree-id) && update-cache --refresh
+checkout-cache -a
+rm -r .git
