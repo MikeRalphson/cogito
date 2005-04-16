70aa2961a2ca4163c231b4e6837cc70aaf13aa26
Petr Baudis <pasky@ucw.cz>
2005-04-16 04:02:44 +0200

    Introduced git update, by which you can "seek" to an arbitrary commit with your working tree.

---
+++ gitupdate.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#
+# Update the working tree to a given commit.
+# Copyright (c) Petr Baudis, 2005
+#
+# This will bring the working tree from its current HEAD to a given
+# commit. Note that it changes just the HEAD of the working tree, not
+# the branch it is corresponding to. Therefore, for a master tree:
+#
+#	git up git-pasky-0.1
+#	git diff this master # will do the correct thing
+#	git up master # will restore what we had before
+#
+# Takes the target commit ID.
+
+dstcommit=$(gitXnormid.sh -c $1)
+
+die () {
+	echo gitupdate.sh: $@ >&2
+	exit 1
+}
+
+
+[ "$dstcommit" ] || die "usage: git update COMMIT_ID"
+
+curcommit=$(commit-id)
+[ "$curcommit" != "$dstcommit" ] || die "nothing to do"
+
+rm .git/HEAD
+if [ -s ".git/heads/$1" ]; then
+	ln -s heads/$1 .git/HEAD
+else
+	echo $dstcommit >.git/HEAD
+fi
+
+read-tree $(tree-id)
+git diff $curcommit $dstcommit | git apply
+update-cache --refresh
+
+echo "On commit $dstcommit"
