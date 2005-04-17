b0e12b1a91be00fac196178c378e3e74d29736e5
Petr Baudis <pasky@ucw.cz>
2005-04-18 00:26:28 +0200

    Killed git clone and made git init accept an optional rsync URL parameter instead.
    Idea by Daniel Barkalow.

--- gitinit.sh
+++ gitinit.sh
@@ -2,6 +2,12 @@
 #
 # Initialize a GIT repository.
 # Copyright (c) Petr Baudis, 2005
+#
+# Takes an optional parameter which will make it "clone" a specified
+# remote repository by pulling it and then making the current tree
+# tracking it.
+
+uri=$1
 
 
 die () {
@@ -15,3 +21,18 @@ init-db
 mkdir .git/heads .git/tags
 touch .git/heads/master
 ln -s heads/master .git/HEAD
+
+if [ "$uri" ]; then
+	echo -e "origin\t$uri" >.git/remotes
+	git pull origin || die "pull failed"
+
+	cp .git/heads/origin .git/heads/master
+	read-tree $(tree-id)
+	checkout-cache -a
+	update-cache --refresh
+
+	# We are tracked by default
+	echo origin >.git/tracking
+
+	echo "Cloned and tracking branch \"origin\" ($uri)"
+fi
