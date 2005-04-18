4c5da8571fd9b4bf8ebd18d19f30d968e58b3292
Petr Baudis <pasky@ucw.cz>
2005-04-18 23:45:32 +0200

    Make git log's date pretty-printed.
    Heavily inspired by signed-off patch of David Woodhouse <dwmw2@infradead.org>,
    and ideas by Sanjoy Mahajan <sanjoy@mrao.cam.ac.uk> and
    Ray Lee <ray-lk@madrabbit.org>.

--- gitlog.sh
+++ gitlog.sh
@@ -13,6 +13,28 @@ base=$(gitXnormid.sh -c $1) || exit 1
 
 rev-tree $base | sort -rn | while read time commit parents; do
 	echo commit ${commit%:*};
-	cat-file commit $commit
+	cat-file commit $commit | \
+		while read key rest; do
+			case "$key" in
+			"author"|"committer")
+				date=(${rest#*> })
+				sec=${date[0]}; tz=${date[1]}
+				dtz=${tz/+/+ }; dtz=${dtz/-/- }
+				pdate="$(date -Rud "1970-01-01 UTC + $sec sec $dtz" 2>/dev/null)"
+				if [ "$pdate" ]; then
+					echo $key $rest | sed "s/>.*/> ${pdate/+0000/$tz}/"
+				else
+					echo $key $rest
+				fi
+				;;
+			"")
+				echo; cat
+				;;
+			*)
+				echo $key $rest
+				;;
+			esac
+
+		done
 	echo -e "\n--------------------------"
 done
