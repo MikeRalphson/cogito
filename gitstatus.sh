8caec85e1410ffddfed6c4c090f212180b0e0c06
Petr Baudis <pasky@ucw.cz>
2005-04-18 16:31:24 +0200

    Fixed displaying added/removed files. Still far from perfect.

--- gitstatus.sh
+++ gitstatus.sh
@@ -5,8 +5,8 @@
 
 {
 	show-files -z -t --others --deleted --unmerged
-	[ -s .git/add-queue ] && cat .git/add-queue | sed 's/^/A /;s/$/\0/'
-	[ -s .git/rm-queue ] && cat .git/rm-queue | sed 's/^/D /;s/$/\0/'
+	[ -s .git/add-queue ] && cat .git/add-queue | sed 's/^/A /' | { read x; echo -ne $x'\0'; }
+	[ -s .git/rm-queue ] && cat .git/rm-queue | sed 's/^/D /' | { read x; echo -ne $x'\0'; }
 } | sort -z -k 2 | xargs -0 sh -c '
 while [ "$1" ]; do
 	tag=$(echo "$1" | cut -d " " -f 1);
