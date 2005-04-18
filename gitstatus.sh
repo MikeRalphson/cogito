fbf1ef918f740f1a8144d75e4df217029ac5ddc9
Petr Baudis <pasky@ucw.cz>
2005-04-18 23:49:33 +0200

    Dramatically speed git status up.

--- gitstatus.sh
+++ gitstatus.sh
@@ -9,8 +9,8 @@
 	[ -s .git/rm-queue ] && cat .git/rm-queue | sed 's/^/D /' | { read x; echo -ne $x'\0'; }
 } | sort -z -k 2 | xargs -0 sh -c '
 while [ "$1" ]; do
-	tag=$(echo "$1" | cut -d " " -f 1);
-	filename=$(echo "$1" | cut -d " " -f 2-);
+	tag=${1% *};
+	filename=${1r* };
 	case "$filename" in
 	*.[ao] | tags | ,,merge*) ;;
 	*)   echo "$tag $filename";;
