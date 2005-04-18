7679c1fa42686e4a89dad159f7c5b3fc85354b3c
Petr Baudis <pasky@ucw.cz>
2005-04-18 04:15:28 +0200

    git fork now allows reusing an existing local branch for the new directory; in that case it is basically git export without getting outside of the git framework with the new tree.

--- gitfork.sh
+++ gitfork.sh
@@ -3,16 +3,17 @@
 # Create a branch sharing the objects database.
 # Copyright (c) Petr Baudis, 2005
 #
-# This script creates a new branch in a given directory, sharing
-# the same objects database with the branch in the current directory,
-# and forking from it at the latest commit. You can use the name
+# This script creates a new branch (or revives an unused old branch)
+# in a given directory, sharing the same objects database with the
+# branch in the current directory, and forking from it at the latest
+# commit (in the case of a new branch). You can use the name
 # of the branch as an ID in all the other branches sharing the
 # objects database.
 #
-# The new directory has a fresh checkout of the current tree.
+# The new directory has a fresh checkout of the branch.
 #
-# Takes the new branch name, its directory name, and potentially
-# the head commit ID.
+# Takes the desired branch name, its directory name, and potentially
+# the head commit ID (for new branch).
 
 name=$1
 destdir=$2
@@ -29,15 +30,15 @@ die () {
 if [ "$name" = "local" ] || [ "$name" = "this" ]; then
 	die "given branch name is reserved"
 fi
-if grep -q $(echo -e "^$name\t" | sed 's/\./\\./g') .git/remotes \
-   || [ -s ".git/heads/$name" ]; then
-	die "branch already exists"
+if grep -q $(echo -e "^$name\t" | sed 's/\./\\./g') .git/remotes; then
+	die "cannot fork remote branch"
 fi
 
 [ -e "$destdir" ] && die "$destdir already exists"
 
 git lntree "$destdir"
-echo $head >.git/heads/$name
+# FIXME: We should allow only forkign unused branches!
+[ -s ".git/heads/$name" ] || echo $head >.git/heads/$name
 ln -s heads/$name "$destdir/.git/HEAD"
 
 cd "$destdir"
