14366089b00b2bbfac092d25d3f374524cb4f045
Petr Baudis <pasky@ucw.cz>
2005-04-18 21:46:59 +0200

    git diff now supports diffing individual files if you are diffing against working tree.

--- gitdiff.sh
+++ gitdiff.sh
@@ -21,6 +21,13 @@ id1=" "
 id2=" "
 parent=
 
+
+die () {
+	echo gitdiff.sh: $@ >&2
+	exit 1
+}
+
+
 # FIXME: The commandline parsing is awful.
 
 if [ "$1" = "-p" ]; then
@@ -53,7 +60,7 @@ if [ "$id2" = " " ]; then
 	fi
 	# FIXME: We should squeeze gitdiff-do-alike output from this.
 	# TODO: Show diffs for added/removed files based on the queues.
-	show-diff -q
+	show-diff -q "$@"
 	if [ "$id1" != " " ]; then
 		read-tree $(tree-id)
 		update-cache --refresh
@@ -64,9 +71,7 @@ fi
 id1=$(gitXnormid.sh "$id1") || exit 1
 id2=$(gitXnormid.sh "$id2") || exit 1
 
-if [ "$id1" = "$id2" ]; then
-	echo "gitdiff.sh: trying to diff $id1 against itself" >&2
-	exit 1
-fi
+[ "$@" ] && die "diffing individual files is not yet supported"
+[ "$id1" = "$id2" ] && die "trying to diff $id1 against itself"
 
 diff-tree -r -z $id1 $id2 | xargs -0 gitdiff-do $id1 $id2
