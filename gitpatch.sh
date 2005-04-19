9b1151e0e13dd02a0c21fd32dc2823fc99c4e0a3
Petr Baudis <pasky@ucw.cz>
2005-04-19 19:49:56 +0200

    Add a git patch command, which will export a given commit  in a patchy format (commit description and diff against first parent). Inspired by Ingo Molnar.

---
+++ gitpatch.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Make a patch from a given commit.
+# Copyright (c) Petr Baudis, 2005
+#
+# Takes commit ID, defaulting to HEAD.
+
+id=$(gitXnormid.sh -c $1) || exit 1
+
+cat-file commit $id
+echo
+git diff -p -r $id
