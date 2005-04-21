8845b1f4368eca747c6cde8f30ee1c6b56d5d8c3
Petr Baudis <pasky@ucw.cz>
2005-04-22 00:42:05 +0200

    git log now can take an optional -c argument which will make its output mildly (and eye-friendly) colorized. Idea by Sean <seanlkml@sympatico.ca>.

--- gitlog.sh
+++ gitlog.sh
@@ -9,32 +9,57 @@
 #
 # Takes an id resolving to a commit to start from (HEAD by default).
 
+if [ "$1" = "-c" ]; then
+	shift
+	colheader=$(setterm -foreground green)
+	colauthor=$(setterm -foreground cyan)
+	colcommitter=$(setterm -foreground magenta)
+	colsignoff=$(setterm -foreground yellow)
+	coldefault=$(setterm -foreground default)
+else
+	colheader=
+	colauthor=
+	colcommitter=
+	colsignoff=
+	coldefault=
+fi
+
 base=$(gitXnormid.sh -c $1) || exit 1
 
 rev-tree $base | sort -rn | while read time commit parents; do
-	echo commit ${commit%:*};
+	echo $colheader commit ${commit%:*} $coldefault;
 	cat-file commit $commit | \
 		while read key rest; do
 			case "$key" in
 			"author"|"committer")
+				if [ "$key" = "author" ]; then
+					color="$colauthor"
+				else
+					color="$colcommitter"
+				fi
+
 				date=(${rest#*> })
 				sec=${date[0]}; tz=${date[1]}
 				dtz=${tz/+/+ }; dtz=${dtz/-/- }
 				pdate="$(date -Rud "1970-01-01 UTC + $sec sec $dtz" 2>/dev/null)"
 				if [ "$pdate" ]; then
-					echo $key $rest | sed "s/>.*/> ${pdate/+0000/$tz}/"
+					echo -n $color $key $rest | sed "s/>.*/> ${pdate/+0000/$tz}/"
+					echo $coldefault
 				else
-					echo $key $rest
+					echo $color $key $rest $coldefault
 				fi
 				;;
 			"")
-				echo; sed 's/^/    /'
+				echo; sed -re '
+					/ *Signed-off-by.*/Is//'$colsignoff'&'$coldefault'/
+					s/^/    /
+				'
 				;;
 			*)
-				echo $key $rest
+				echo $colheader $key $rest $coldefault
 				;;
 			esac
 
 		done
 	echo
-done | ${PAGER:-less}
+done | ${PAGER:-less} ${PAGER_FLAGS:--R}
