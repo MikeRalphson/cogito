3524031d34f52f9607b13e29354f2d6416b78241
Petr Baudis <pasky@ucw.cz>
2005-04-22 01:57:36 +0200

    git patch now can take a range of commits instead of a single commit, and in that case emits patches for all the commits in the range.

--- gitpatch.sh
+++ gitpatch.sh
@@ -3,11 +3,38 @@
 # Make a patch from a given commit.
 # Copyright (c) Petr Baudis, 2005
 #
-# Takes commit ID, defaulting to HEAD.
+# Takes commit ID, defaulting to HEAD, or id1:id2, forming a range
+# (id1;id2]. (Use "id1:" to take just everything from id1 to HEAD.)
 
-id=$(gitXnormid.sh -c $1) || exit 1
 
-echo commit $id
-cat-file commit $id
-echo
-git diff -p -r $id
+showpatch () {
+	id=$1
+	echo commit $id
+	cat-file commit $id | while read line; do
+		echo $line
+		[ ! "$line" ] && sed 's/^/    /'
+	done
+	echo
+	git diff -p -r $id
+}
+
+
+if echo "$1" | grep -q ':'; then
+	id1=$(gitXnormid.sh -c $(echo "$1" | cut -d : -f 1)) || exit 1
+	id2=$(gitXnormid.sh -c $(echo "$1" | cut -d : -f 2)) || exit 1
+
+	rev-tree $id2 ^$id1 | while read time commit rest; do
+		id=$(echo $commit | cut -d : -f 1)
+		showpatch $id
+		echo
+		echo
+		echo -e '\014'
+		echo '!-------------------------------------------------------------flip-'
+		echo
+		echo
+	done
+
+else
+	id=$(gitXnormid.sh -c $1) || exit 1
+	showpatch $id
+fi
