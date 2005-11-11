5dc010518b6fe3678a7649bd3e2a65e880628060
Petr Baudis <pasky@suse.cz>
2005-11-12 00:14:56 +0100

    neutral.sh now supports the field selectors
    That is -s <field>,<field>,... or +... in the interactive mode will
    enable you to select only some fields to show at the output. This is
    useful when you want to prevent it from going at lengths expensively
    computing some stuff you are not going to use anyway.

--- contrib/neutral.sh
+++ contrib/neutral.sh
@@ -8,8 +8,24 @@ die() {
 	exit 1
 }
 
+# Take a field name as an argument and check in the $interesting[] array
+# whether we should output it.
+interesting() {
+	[ "${interesting[0]}" = '*' ] && return 0
+	for field in "${interesting[@]}"; do
+		[ "$field" = "$1" ] && return 0
+	done
+	return 1
+}
+
+
 if [ ! "$1" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
 	die http://revctrl.org/NeutralInterface
+fi
+
+interesting=('*')
+if [ "$1" = "-s" ]; then
+	shift; interesting=(${1//,/ }); shift
 fi
 
 cmd="$1"; shift; args=("$@");
@@ -21,40 +37,51 @@ while [ "$cmd" ]; do
 		: # nothing
 
 	elif [ "$cmd" = "identify" ]; then
-		echo type:3:git
+		interesting "type" && echo "type:3:git"
 
 	elif [ "$cmd" = "changeset" ]; then
 		[ "${args[0]}" ] || die "missing changeset id"
 		commit="$(git-rev-parse --verify "${args[0]}")" || die "bad changeset id"
-		echo identifier:40:"$commit"
+		interesting "identifier" && echo "identifier:40:$commit"
 
+		interesting "parent" &&
 		for parent in $(git-rev-parse --verify "$commit"^); do
-			echo parent:40:$parent
+			echo "parent:40:$parent"
 		done
 
+		interesting "tag" &&
 		grep -r -l "$commit" ${GIT_DIR:-.git}/refs/tags |
 			while IFS=$'\n' read tag; do
 				tag="${tag##*/}"
-				echo tag:${#tag}:"$tag"
+				echo "tag:${#tag}:$tag"
 			done
 
 		# FIXME: doesn't handle filenames containing newlines
+		interesting "file" &&
 		echo $commit $(git-rev-parse --verify "$commit"^) |
 			git-diff-tree -r -m --stdin | grep ^: | cut -f 2- |
 			while IFS=$'\n' read file; do
-				echo file:${#file}:"$file"
+				echo "file:${#file}:$file"
 			done
 
-		author="$(git-cat-file commit "$commit" | sed -n 's/^author //p;/^$/q')"
+		if interesting "user" || interesting "date"; then
+			author="$(git-cat-file commit "$commit" | sed -n 's/^author //p;/^$/q')"
 
-		user="$(echo "$author" | sed 's/\([^<]* <[^>]*>\).*/\1/')"
-		echo user:${#user}:"$user"
+			if interesting "user"; then
+				user="$(echo "$author" | sed 's/\([^<]* <[^>]*>\).*/\1/')"
+				echo user:${#user}:"$user"
+			fi
 
-		date="$(echo "$author" | sed 's/[^<]* <[^>]*> \(.*\)/\1/')"
-		echo date:${#date}:"$date"
+			if interesting "date"; then
+				date="$(echo "$author" | sed 's/[^<]* <[^>]*> \(.*\)/\1/')"
+				echo date:${#date}:"$date"
+			fi
+		fi
 
-		desc="$(git-cat-file commit "$commit" | sed -n '/^$/{:a n;p;b a}')"
-		echo description:${#desc}:"$desc"
+		if interesting "description"; then
+			desc="$(git-cat-file commit "$commit" | sed -n '/^$/{:a n;p;b a}')"
+			echo description:${#desc}:"$desc"
+		fi
 
 	else
 		die "unknown command"
@@ -64,6 +91,14 @@ while [ "$cmd" ]; do
 	if [ "$interactive" ]; then
 		echo # terminate output
 		read cmd
+		interesting=('*')
+		cmdname="${cmd%%+*}"
+		if [ "$cmdname" != "$cmd" ]; then
+			fields="${cmd#*+}"
+			interesting=(${fields//,/ })
+			cmd="$cmdname"
+		fi
+
 		args=()
 		while true; do
 			# do with bash, I guess.
